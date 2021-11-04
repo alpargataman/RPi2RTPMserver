66e5059b55afc8f526be9db94175e622cba3bc49
Cristian Bastidas <6288158+crixodia@users.noreply.github.com>
2021-11-04 01:54:31 -0500

    Optimal defaults

--- stream.sh
+++ stream.sh
@@ -2,7 +2,7 @@
 # Configuration of ffmpeg
 
 VBR="3000k"                                    # Video bitrate
-FPS="30"                                       # Video FPS
+FPS="24"                                       # Video FPS up to 30
 QUAL="ultrafast"                               # FFMPEG preset quality
 RESOLUTION="848x480"                           # Video resolution
 YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"  # RTMP server URL
@@ -13,13 +13,14 @@ MARQUEE="marquee.txt"                          # Path to the TXT with a marquee 
 FONT="FreeSerif.ttf"                           # Path to the font
 FONT_SIZE="20"                                 # Font size 
 THREADS="4"                                    # Number of threadsusing to encode video and audio
-THREAD_QUEUE="2048"                            # Thread queue size for audio
+THREAD_QUEUE="1024"                            # Thread queue size for audio
 CRF="20"                                       # Constant rate factor of the video
 KEY="xxxx-xxxx-xxxx-xxxx"                      # Stream key 
 
 # infinite loop
 while [ 1 ]
 do
+# change -> alsa is you are using it
 ffmpeg \
   -stream_loop -1 -i $VIDEO \
   -thread_queue_size $THREAD_QUEUE -f pulse -i default \
