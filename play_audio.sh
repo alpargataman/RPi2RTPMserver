03ee4bcd9599b264e3b955548908115bfc790abc
Cristian Bastidas <gabrielbastidas5g@outlook.com>
2021-11-05 12:28:48 -0500

    Better feedback messages

--- play_audio.sh
+++ play_audio.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-
 FOLDER=mp3              # Folder with songs
 COVER_SIZE="200"    	# Song cover size
 INTERNET_COVER=0        # If cover doesn't exist download it form the internet [1|0]
 
 while :
 do
-    # Deleting old cover
-    rm "cover.png"
+    echo "Deleting old cover "; rm "cover.png"
     
-    # Lookig for next song in the folder
+    echo "Lookig for next song in the folder"
     mapfile -t my_array < <(find $FOLDER -iname "*.mp3")
     
-    # Choosing next song randomly
+    echo "Choosing next song randomly"
     SONG=${my_array[ RANDOM % ${#my_array[@]}]}
     
     # Saving next song data in variables for later use
@@ -21,7 +19,7 @@ do
     ALBUM=$(exiftool "$SONG" | grep "Album  " | cut -f2 -d':')
     TITLE=$(exiftool "$SONG" | grep "Title  " | cut -f2 -d':')
     
-    # Changing song title in the video
+    # Changing song info in the video
     echo $ARTIST > livetext.txt
     echo $ALBUM >> livetext.txt
     echo $TITLE >> livetext.txt
@@ -34,7 +32,8 @@ do
     mplayer -af volnorm=2:0.05 "$SONG" &
     
     # Taking next song's cover from file
-    ffmpeg -i "$SONG" cover.jpg
+    # Remove -hide_banner -loglevel error if you want the whole ffmpeg feedback
+    ffmpeg -hide_banner -loglevel error -i "$SONG" cover.jpg
     if [ -f "cover.jpg" ]; then
         convert cover.jpg -resize "$COVER_SIZEx$COVER_SIZE"\> cover.png
         rm cover.jpg
